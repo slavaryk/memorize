6d7d72932677824ad8261d6a43a4e7f578c4bc2f
mpq <m3atyy@gmail.com>
2022-11-08 22:14:02 +0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Слава Рыков on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
