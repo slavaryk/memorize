10cae8cd2e53b0d1bdff3df84fd6862eca3afd1e
mpq <m3atyy@gmail.com>
2022-11-12 18:11:38 +0500

    feat: working on CardView

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let CARDS_CONTENT_LIST = ["⚽️", "🏀", "🏐", "⚾️"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(CARDS_CONTENT_LIST, id: \.self)
+            {
+                content in
+                CardView(CONTENT: content)
+            }
         }
-        .padding()
+        .padding(.horizontal )
+    }
+}
+
+struct CardView: View {
+    let CONTENT: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let CARD_SHAPE  = RoundedRectangle(cornerRadius: 20)
+
+            if isFaceUp {
+                CARD_SHAPE.fill().foregroundColor(.white)
+                CARD_SHAPE.stroke(lineWidth: 3).foregroundColor(.blue)
+                Text(CONTENT).font(.largeTitle)
+            } else {
+                CARD_SHAPE.fill().foregroundColor(.blue)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.light)
     }
 }
