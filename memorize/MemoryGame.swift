ea5317b3c2efec97c4a0e1df9aabfdc01179e8d2
mpq <m3atyy@gmail.com>
2022-11-19 19:14:54 +0500

    feat: wip on model

--- memorize/MemoryGame.swift
+++ memorize/MemoryGame.swift
@@ -8,15 +8,24 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: [Card] = []
+    private(set) var cards: [Card] = []
+    
+    init(numberOfPairsOfCards: Int, content: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for index in 0..<numberOfPairsOfCards {
+            cards.append(Card(content: content(index)))
+            cards.append(Card(content: content(index)))
+        }
+    }
     
     func choose(_ card: Card) {
         
     }
     
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
     }
 }
