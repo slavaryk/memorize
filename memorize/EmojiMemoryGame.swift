7f7947accf4bf77bc471939bacaaf7663205b6c9
mpq <m3atyy@gmail.com>
2022-11-19 19:15:06 +0500

    feat: wip on view-model

--- memorize/EmojiMemoryGame.swift
+++ memorize/EmojiMemoryGame.swift
@@ -6,3 +6,25 @@
 //
 
 import SwiftUI
+
+class EmojiMemoryGame {
+    static let emojis: [[String]] = [
+        ["⚽️", "🏀", "🏐", "⚾️", "🎱", "🎾", "🏉", "🏈"],
+        ["🛸", "🚀", "🛰️", "🚁", "🛩️", "🛶", "⛵️", "🚤"],
+        ["⌚️", "📱", "💻", "⌨️", "📷", "🎥", "📟", "🎙️"],
+    ]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { index in
+            EmojiMemoryGame.emojis[0][index]
+        }
+    }
+
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    var cards: [MemoryGame<String>.Card] {
+        model.cards
+    }
+    
+    
+}
