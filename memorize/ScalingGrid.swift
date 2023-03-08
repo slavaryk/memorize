3109f02b1738c9d52fec7f5ca81ccbe5ccfa25ca
mpq <m3atyy@gmail.com>
2023-03-08 12:12:26 +0500

    refactor: reposition functions

--- memorize/ScalingGrid.swift
+++ memorize/ScalingGrid.swift
@@ -27,10 +27,6 @@ struct ScalingGrid<Item, ItemView>: View where Item: Identifiable, ItemView: Vie
         }
     }
     
-    private func adaptiveGridItem(width: CGFloat) -> GridItem {
-        GridItem(.adaptive(minimum: width))
-    }
-    
     private func getScaledWidth(dependsOn itemsCount: Int, in size: CGSize, with aspectRatio: CGFloat) -> CGFloat {
         var columnCount = 1
         var rowCount = itemsCount
@@ -52,5 +48,9 @@ struct ScalingGrid<Item, ItemView>: View where Item: Identifiable, ItemView: Vie
         }
         
         return floor(size.width / CGFloat(columnCount))
+    }
+    
+    private func adaptiveGridItem(width: CGFloat) -> GridItem {
+        GridItem(.adaptive(minimum: width))
     }
 }
